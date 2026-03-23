Rikpat/cilium

commit 72e5ca6c0ba2496e78c82112abb797ee050eb8b4

datapath: use direct routing device cell
Removes the device manager shim as the only functionality that remained
was choosing the direct routing device. That logic is now contained in
the direct routing device cell, which is used treewide to find the
device to use.

Adds the direct routing device to the LocalNodeConfiguration used in the
loader and related subsystems.

Signed-off-by: David Bimmler <david.bimmler@isovalent.com>
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/datapath/linux/config/config.go
@@ -571,23 +571,42 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	cDefinesMap["HASH_INIT6_SEED"] = fmt.Sprintf("%d", maglev.SeedJhash1)
 
 	if option.Config.DirectRoutingDeviceRequired() {
+		drd := cfg.DirectRoutingDevice
 		if option.Config.EnableIPv4 {
-			ifindex, ip, ok := h.nodeAddressing.IPv4().DirectRouting()
-			if !ok {
+			if drd == nil {
 				return fmt.Errorf("IPv4 direct routing device not found")
 			}
-			ipv4 := byteorder.NetIPv4ToHost32(ip)
+			var ipv4 uint32
+			for _, addr := range drd.Addrs {
+				if addr.Addr.Is4() {
+					ipv4 = byteorder.NetIPv4ToHost32(addr.AsIP())
+					break
+				}
+			}
+			if ipv4 == 0 {
+				return fmt.Errorf("IPv4 direct routing device IP not found")
+			}
 			cDefinesMap["IPV4_DIRECT_ROUTING"] = fmt.Sprintf("%d", ipv4)
-			cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = fmt.Sprintf("%d", ifindex)
+			cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = fmt.Sprintf("%d", drd.Index)
 		}
 		if option.Config.EnableIPv6 {
-			ifindex, ip, ok := h.nodeAddressing.IPv6().DirectRouting()
-			if !ok {
+			if drd == nil {
 				return fmt.Errorf("IPv6 direct routing device not found")
+			}
+
+			var ip net.IP
+			for _, addr := range drd.Addrs {
+				if addr.Addr.Is6() {
+					ip = addr.AsIP()
+					break
+				}
+			}
+			if ip.IsUnspecified() {
+				return fmt.Errorf("IPv6 direct routing device IP not found")
 			}
 			extraMacrosMap["IPV6_DIRECT_ROUTING"] = ip.String()
 			fw.WriteString(FmtDefineAddress("IPV6_DIRECT_ROUTING", ip))
-			cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = fmt.Sprintf("%d", ifindex)
+			cDefinesMap["DIRECT_ROUTING_DEV_IFINDEX"] = fmt.Sprintf("%d", drd.Index)
 		}
 	} else {
 		var directRoutingIPv6 net.IP
@@ -1056,10 +1075,10 @@ func (h *HeaderfileWriter) WriteEndpointConfig(w io.Writer, cfg *datapath.LocalN
 	writeIncludes(w)
 	h.writeStaticData(deviceNames, fw, e)
 
-	return h.writeTemplateConfig(fw, deviceNames, cfg.HostEndpointID, e)
+	return h.writeTemplateConfig(fw, deviceNames, cfg.HostEndpointID, e, cfg.DirectRoutingDevice)
 }
 
-func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []string, hostEndpointID uint64, e datapath.EndpointConfiguration) error {
+func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []string, hostEndpointID uint64, e datapath.EndpointConfiguration, drd *tables.Device) error {
 	if e.RequireEgressProg() {
 		fmt.Fprintf(fw, "#define USE_BPF_PROG_FOR_INGRESS_POLICY 1\n")
 	}
@@ -1068,16 +1087,13 @@ func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []strin
 		fmt.Fprintf(fw, "#define ENABLE_ROUTING 1\n")
 	}
 
-	if !option.Config.EnableHostLegacyRouting && option.Config.DirectRoutingDevice != "" {
-		directRoutingIface := option.Config.DirectRoutingDevice
-		directRoutingIfIndex, err := link.GetIfIndex(directRoutingIface)
-		if err != nil {
-			return err
-		}
-		fmt.Fprintf(fw, "#define DIRECT_ROUTING_DEV_IFINDEX %d\n", directRoutingIfIndex)
-		if len(devices) == 1 {
-			if e.IsHost() || !option.Config.EnforceLXCFibLookup() {
-				fmt.Fprintf(fw, "#define ENABLE_SKIP_FIB 1\n")
+	if !option.Config.EnableHostLegacyRouting {
+		if drd != nil {
+			fmt.Fprintf(fw, "#define DIRECT_ROUTING_DEV_IFINDEX %d\n", drd.Index)
+			if len(devices) == 1 {
+				if e.IsHost() || !option.Config.EnforceLXCFibLookup() {
+					fmt.Fprintf(fw, "#define ENABLE_SKIP_FIB 1\n")
+				}
 			}
 		}
 	}
@@ -1117,5 +1133,5 @@ func (h *HeaderfileWriter) writeTemplateConfig(fw *bufio.Writer, devices []strin
 // WriteTemplateConfig writes the BPF configuration for the template to a writer.
 func (h *HeaderfileWriter) WriteTemplateConfig(w io.Writer, cfg *datapath.LocalNodeConfiguration, e datapath.EndpointConfiguration) error {
 	fw := bufio.NewWriter(w)
-	return h.writeTemplateConfig(fw, cfg.DeviceNames(), cfg.HostEndpointID, e)
+	return h.writeTemplateConfig(fw, cfg.DeviceNames(), cfg.HostEndpointID, e, cfg.DirectRoutingDevice)
 }

pkg/datapath/linux/config/config_test.go
@@ -92,6 +92,7 @@ func writeConfig(t *testing.T, header string, write writeFn) {
 		t.Logf("  Testing %s configuration: %s", header, test.description)
 		h := hive.New(
 			provideNodemap,
+			tables.DirectRoutingDeviceCell,
 			cell.Provide(
 				fakeTypes.NewNodeAddressing,
 				func() sysctl.Sysctl { return sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc") },

pkg/option/config.go
@@ -1425,20 +1425,19 @@ type DaemonConfig struct {
 	// after.
 	shaSum [32]byte
 
-	CreationTime        time.Time
-	BpfDir              string   // BPF template files directory
-	LibDir              string   // Cilium library files directory
-	RunDir              string   // Cilium runtime directory
-	ExternalEnvoyProxy  bool     // Whether Envoy is deployed as external DaemonSet or not
-	DirectRoutingDevice string   // Direct routing device (used by BPF NodePort and BPF Host Routing)
-	LBDevInheritIPAddr  string   // Device which IP addr used by bpf_host devices
-	EnableXDPPrefilter  bool     // Enable XDP-based prefiltering
-	XDPMode             string   // XDP mode, values: { xdpdrv | xdpgeneric | none }
-	EnableTCX           bool     // Enable attaching endpoint programs using tcx if the kernel supports it
-	HostV4Addr          net.IP   // Host v4 address of the snooping device
-	HostV6Addr          net.IP   // Host v6 address of the snooping device
-	EncryptInterface    []string // Set of network facing interface to encrypt over
-	EncryptNode         bool     // Set to true for encrypting node IP traffic
+	CreationTime       time.Time
+	BpfDir             string   // BPF template files directory
+	LibDir             string   // Cilium library files directory
+	RunDir             string   // Cilium runtime directory
+	ExternalEnvoyProxy bool     // Whether Envoy is deployed as external DaemonSet or not
+	LBDevInheritIPAddr string   // Device which IP addr used by bpf_host devices
+	EnableXDPPrefilter bool     // Enable XDP-based prefiltering
+	XDPMode            string   // XDP mode, values: { xdpdrv | xdpgeneric | none }
+	EnableTCX          bool     // Enable attaching endpoint programs using tcx if the kernel supports it
+	HostV4Addr         net.IP   // Host v4 address of the snooping device
+	HostV6Addr         net.IP   // Host v6 address of the snooping device
+	EncryptInterface   []string // Set of network facing interface to encrypt over
+	EncryptNode        bool     // Set to true for encrypting node IP traffic
 
 	// If set to true the daemon will detect new and deleted datapath devices
 	// at runtime and reconfigure the datapath to load programs onto the new
@@ -3037,7 +3036,6 @@ func (c *DaemonConfig) Populate(vp *viper.Viper) {
 	c.DatapathMode = vp.GetString(DatapathMode)
 	c.Debug = vp.GetBool(DebugArg)
 	c.DebugVerbose = vp.GetStringSlice(DebugVerbose)
-	c.DirectRoutingDevice = vp.GetString(DirectRoutingDevice)
 	c.EnableIPv4 = vp.GetBool(EnableIPv4Name)
 	c.EnableIPv6 = vp.GetBool(EnableIPv6Name)
 	c.EnableIPv6NDP = vp.GetBool(EnableIPv6NDPName)

daemon/cmd/daemon.go
@@ -106,8 +106,9 @@ type Daemon struct {
 	monitorAgent monitoragent.Agent
 	ciliumHealth *health.CiliumHealth
 
-	devices   statedb.Table[*datapathTables.Device]
-	nodeAddrs statedb.Table[datapathTables.NodeAddress]
+	directRoutingDev datapathTables.DirectRoutingDevice
+	devices          statedb.Table[*datapathTables.Device]
+	nodeAddrs        statedb.Table[datapathTables.NodeAddress]
 
 	// dnsNameManager tracks which api.FQDNSelector are present in policy which
 	// apply to locally running endpoints.
@@ -382,6 +383,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		compilationLock:   params.CompilationLock,
 		mtuConfig:         params.MTU,
 		datapath:          params.Datapath,
+		directRoutingDev:  params.DirectRoutingDevice,
 		devices:           params.Devices,
 		nodeAddrs:         params.NodeAddrs,
 		nodeDiscovery:     params.NodeDiscovery,
@@ -626,20 +628,26 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 	// This is because the device detection requires self (Cilium)Node object,
 	// and the k8s service watcher depends on option.Config.EnableNodePort flag
 	// which can be modified after the device detection.
-	var devices []string
-	if params.DeviceManager != nil {
-		if devices, err = params.DeviceManager.Detect(params.Clientset.IsEnabled()); err != nil {
-			if option.Config.AreDevicesRequired() {
-				// Fail hard if devices are required to function.
-				return nil, nil, fmt.Errorf("failed to detect devices: %w", err)
-			}
-			log.WithError(err).Warn("failed to detect devices, disabling BPF NodePort")
-			disableNodePort()
+
+	rxn := d.db.ReadTxn()
+	drdName := ""
+	directRoutingDevice, _ := params.DirectRoutingDevice.Get(ctx, rxn)
+	if directRoutingDevice == nil {
+		if option.Config.AreDevicesRequired() {
+			// Fail hard if devices are required to function.
+			return nil, nil, fmt.Errorf("unable to determine direct routing device. Use --%s to specify it",
+				option.DirectRoutingDevice)
 		}
+
+		log.WithError(err).Warn("failed to detect devices, disabling BPF NodePort")
+		disableNodePort()
+	} else {
+		drdName = directRoutingDevice.Name
+		log.WithField(option.DirectRoutingDevice, drdName).Info("Direct routing device detected")
 	}
 
-	nativeDevices, _ := datapathTables.SelectedDevices(d.devices, d.db.ReadTxn())
-	if err := finishKubeProxyReplacementInit(params.Sysctl, nativeDevices); err != nil {
+	nativeDevices, _ := datapathTables.SelectedDevices(d.devices, rxn)
+	if err := finishKubeProxyReplacementInit(params.Sysctl, nativeDevices, drdName); err != nil {
 		log.WithError(err).Error("failed to finalise LB initialization")
 		return nil, nil, fmt.Errorf("failed to finalise LB initialization: %w", err)
 	}
@@ -676,7 +684,7 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 		log.Error("IPv4 and IPv6 masquerading are both disabled, BPF masquerading requires at least one to be enabled")
 		return nil, nil, fmt.Errorf("BPF masquerade requires (--%s=\"true\" or --%s=\"true\")", option.EnableIPv4Masquerade, option.EnableIPv6Masquerade)
 	}
-	if len(devices) == 0 {
+	if len(nativeDevices) == 0 {
 		if option.Config.EnableHostFirewall {
 			msg := "Host firewall's external facing device could not be determined. Use --%s to specify."
 			log.WithError(err).Errorf(msg, option.Devices)

daemon/cmd/daemon_main.go
@@ -248,9 +248,6 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.StringSlice(option.DebugVerbose, []string{}, "List of enabled verbose debug groups")
 	option.BindEnv(vp, option.DebugVerbose)
 
-	flags.String(option.DirectRoutingDevice, "", "Device name used to connect nodes in direct routing mode (used by BPF NodePort, BPF host routing; if empty, automatically set to a device with k8s InternalIP/ExternalIP or with a default route)")
-	option.BindEnv(vp, option.DirectRoutingDevice)
-
 	flags.Bool(option.EnableRuntimeDeviceDetection, true, "Enable runtime device detection and datapath reconfiguration (experimental)")
 	option.BindEnv(vp, option.EnableRuntimeDeviceDetection)
 	flags.MarkDeprecated(option.EnableRuntimeDeviceDetection, "Runtime device detection and datapath reconfiguration is now the default and only mode of operation")
@@ -1686,9 +1683,9 @@ type daemonParams struct {
 	APILimiterSet          *rate.APILimiterSet
 	AuthManager            *auth.AuthManager
 	Settings               cellSettings
-	DeviceManager          *linuxdatapath.DeviceManager `optional:"true"`
 	Devices                statedb.Table[*datapathTables.Device]
 	NodeAddrs              statedb.Table[datapathTables.NodeAddress]
+	DirectRoutingDevice    datapathTables.DirectRoutingDevice
 	// Grab the GC object so that we can start the CT/NAT map garbage collection.
 	// This is currently necessary because these maps have not yet been modularized,
 	// and because it depends on parameters which are not provided through hive.

daemon/cmd/ipam.go
@@ -537,7 +537,12 @@ func (d *Daemon) configureIPAM() {
 		node.SetIPv6NodeRange(allocCIDR)
 	}
 
-	if err := node.AutoComplete(); err != nil {
+	device := ""
+	drd, _ := d.directRoutingDev.Get(d.ctx, d.db.ReadTxn())
+	if drd != nil {
+		device = drd.Name
+	}
+	if err := node.AutoComplete(device); err != nil {
 		log.WithError(err).Fatal("Cannot autocomplete node addresses")
 	}
 }

daemon/cmd/kube_proxy_replacement.go
@@ -344,7 +344,7 @@ func probeKubeProxyReplacementOptions(sysctl sysctl.Sysctl) error {
 
 // finishKubeProxyReplacementInit finishes initialization of kube-proxy
 // replacement after all devices are known.
-func finishKubeProxyReplacementInit(sysctl sysctl.Sysctl, devices []*tables.Device) error {
+func finishKubeProxyReplacementInit(sysctl sysctl.Sysctl, devices []*tables.Device, directRoutingDevice string) error {
 	if !option.Config.EnableNodePort {
 		// Make sure that NodePort dependencies are disabled
 		disableNodePort()
@@ -413,6 +413,7 @@ func finishKubeProxyReplacementInit(sysctl sysctl.Sysctl, devices []*tables.Devi
 	if option.Config.EnableIPv4 &&
 		!option.Config.TunnelingEnabled() &&
 		option.Config.LoadBalancerUsesDSR() &&
+		directRoutingDevice != "" &&
 		len(devices) > 1 {
 
 		// In the case of the multi-dev NodePort DSR, if a request from an
@@ -422,15 +423,14 @@ func finishKubeProxyReplacementInit(sysctl sysctl.Sysctl, devices []*tables.Devi
 		// and the client IP is reachable via other device than the direct
 		// routing one.
 
-		iface := option.Config.DirectRoutingDevice
-		if val, err := sysctl.Read(fmt.Sprintf("net.ipv4.conf.%s.rp_filter", iface)); err != nil {
+		if val, err := sysctl.Read(fmt.Sprintf("net.ipv4.conf.%s.rp_filter", directRoutingDevice)); err != nil {
 			log.Warnf("Unable to read net.ipv4.conf.%s.rp_filter: %s. Ignoring the check",
-				iface, err)
+				directRoutingDevice, err)
 		} else {
 			if val == "1" {
 				log.Warnf(`DSR might not work for requests sent to other than %s device. `+
 					`Run 'sysctl -w net.ipv4.conf.%s.rp_filter=2' (or set to '0') on each node to fix`,
-					iface, iface)
+					directRoutingDevice, directRoutingDevice)
 			}
 		}
 	}

daemon/cmd/local_node_sync.go
@@ -99,10 +99,6 @@ func (ini *localNodeSynchronizer) initFromConfig(ctx context.Context, n *node.Lo
 	n.IPv4NativeRoutingCIDR = ini.Config.IPv4NativeRoutingCIDR
 	n.IPv6NativeRoutingCIDR = ini.Config.IPv6NativeRoutingCIDR
 
-	// If there is one device specified, use it to derive better default
-	// allocation prefixes
-	node.SetDefaultPrefix(ini.Config, ini.Config.DirectRoutingDevice, n)
-
 	// Initialize node IP addresses from configuration.
 	if ini.Config.IPv6NodeAddr != "auto" {
 		if ip := net.ParseIP(ini.Config.IPv6NodeAddr); ip == nil {

commit b7c781e1f816ba3e0bf85eb8609b5e78a3ea5c64

config: Tolerate partial address loss on direct routing device
When the direct routing device temporarily loses an address on one
stack (e.g. IPv4 during DHCP renewal), WriteNodeConfig errors and
blocks the entire Reinitialize path. This prevents endpoint
regeneration, so unrelated config changes like policyAuditMode
cannot take effect.

Replace the per-stack hard errors with a single check after both
stacks are evaluated. If at least one stack has a usable address,
proceed with regeneration (the missing stack gets a zero value).
If the device has no usable addresses at all (e.g. both stacks
lost), still error and let the retry loop pick it up once addresses
return.

Fixes: 72e5ca6c0ba ("datapath: use direct routing device cell")

Suggested-by: Robin Gögge <robin.goegge@isovalent.com>
Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/datapath/linux/config/config.go
@@ -381,6 +381,7 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 	// See orchestrator/localnodeconfig.go
 	drd := cfg.DirectRoutingDevice
 	if drd != nil {
+		var hasAddr bool
 		if option.Config.EnableIPv4 {
 			var ipv4 uint32
 			for _, addr := range drd.Addrs {
@@ -389,18 +390,21 @@ func (h *HeaderfileWriter) WriteNodeConfig(w io.Writer, cfg *datapath.LocalNodeC
 					break
 				}
 			}
-			if ipv4 == 0 {
-				return fmt.Errorf("IPv4 direct routing device IP not found")
+			if ipv4 != 0 {
+				hasAddr = true
 			}
 			cDefinesMap["IPV4_DIRECT_ROUTING"] = fmt.Sprintf("%d", ipv4)
 		}
 		if option.Config.EnableIPv6 {
 			ip := preferredIPv6Address(drd.Addrs)
-			if ip.IsUnspecified() {
-				return fmt.Errorf("IPv6 direct routing device IP not found")
+			if ip.IsValid() && !ip.IsUnspecified() {
+				hasAddr = true
 			}
 			extraMacrosMap["IPV6_DIRECT_ROUTING"] = ip.String()
 			fw.WriteString(FmtDefineAddress("IPV6_DIRECT_ROUTING", ip.AsSlice()))
+		}
+		if !hasAddr {
+			return fmt.Errorf("direct routing device %s has no addresses", drd.Name)
 		}
 	} else {
 		var directRoutingIPv6 net.IP

pkg/datapath/linux/config/config_test.go
@@ -469,6 +469,111 @@ func TestPrivilegedNewHeaderfileWriter(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestPrivilegedWriteNodeConfigPartialAddr(t *testing.T) {
+	testutils.PrivilegedTest(t)
+	ns := netns.NewNetNS(t)
+	setupCiliumDummyDevices(t, ns)
+	err := ns.Do(func() error {
+		setupConfigSuite(t)
+
+		cfg, err := NewHeaderfileWriter(WriterParams{
+			NodeAddressing: fakeTypes.NewNodeAddressing(),
+			Sysctl:         sysctl.NewDirectSysctl(afero.NewOsFs(), "/proc"),
+			NodeMap:        fake.NewFakeNodeMapV2(),
+		})
+		require.NoError(t, err)
+
+		tests := []struct {
+			name    string
+			addrs   []tables.DeviceAddress
+			wantErr bool
+		}{
+			{
+				name: "both addresses",
+				addrs: []tables.DeviceAddress{
+					{Addr: ipv4DummyAddr},
+					{Addr: ipv6DummyAddr},
+				},
+			},
+			{
+				name: "only IPv4",
+				addrs: []tables.DeviceAddress{
+					{Addr: ipv4DummyAddr},
+				},
+			},
+			{
+				name: "only IPv6",
+				addrs: []tables.DeviceAddress{
+					{Addr: ipv6DummyAddr},
+				},
+			},
+			{
+				name:    "no addresses",
+				addrs:   nil,
+				wantErr: true,
+			},
+			{
+				name: "only unspecified IPv4",
+				addrs: []tables.DeviceAddress{
+					{Addr: netip.MustParseAddr("0.0.0.0")},
+				},
+				wantErr: true,
+			},
+			{
+				name: "only unspecified IPv6",
+				addrs: []tables.DeviceAddress{
+					{Addr: netip.MustParseAddr("::")},
+				},
+				wantErr: true,
+			},
+			{
+				name: "both unspecified",
+				addrs: []tables.DeviceAddress{
+					{Addr: netip.MustParseAddr("0.0.0.0")},
+					{Addr: netip.MustParseAddr("::")},
+				},
+				wantErr: true,
+			},
+			{
+				name: "valid IPv4 and unspecified IPv6",
+				addrs: []tables.DeviceAddress{
+					{Addr: ipv4DummyAddr},
+					{Addr: netip.MustParseAddr("::")},
+				},
+			},
+			{
+				name: "unspecified IPv4 and valid IPv6",
+				addrs: []tables.DeviceAddress{
+					{Addr: netip.MustParseAddr("0.0.0.0")},
+					{Addr: ipv6DummyAddr},
+				},
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				nodeCfg := dummyNodeCfg
+				nodeCfg.DirectRoutingDevice = &tables.Device{
+					Name:  "eth0",
+					Index: 2,
+					Addrs: tt.addrs,
+				}
+
+				var buf bytes.Buffer
+				err := cfg.WriteNodeConfig(&buf, &nodeCfg)
+				if tt.wantErr {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
+				}
+			})
+		}
+
+		return nil
+	})
+	require.NoError(t, err)
+}
+
 var provideNodemap = cell.Provide(func() nodemap.MapV2 {
 	return fake.NewFakeNodeMapV2()
 })